9081c457576bc040a14bdadbee2d2c437a4835d0
Franco Serio <francoagustinserio@gmail.com>
2023-04-05 18:58:22 -0300

    remove duplicated tabs

--- background.js
+++ background.js
@@ -13,12 +13,32 @@ async function removeAllExceptCurrentTab(tabsIds, currentTabId) {
   await chrome.tabs.remove(allTabIdsExceptCurrent);
 }
 
+async function close_duplicated_tabs() {
+  const allTabs = await getAllTabs();
+  const uniqUrls = new Set(allTabs.map(tab => tab.url));
+  const tabIdsToRemove = [];
+  allTabs.map(tab => {
+    if (!uniqUrls.has(tab.url)) {
+      tabIdsToRemove.push(tab.id);
+    } else {
+      uniqUrls.delete(tab.url);
+    }
+  })
+  await chrome.tabs.remove(tabIdsToRemove);
+}
+
+async function close_all_other_tabs() {
+  const tab = await getCurrentTab();
+  const allTabs = await getAllTabs();
+  const allTabsIds = allTabs.map(tab => tab.id);
+  await removeAllExceptCurrentTab(allTabsIds, tab.id);
+}
+
 chrome.commands.onCommand.addListener(async (command) => {
   if (command == 'close_all_other_tabs') {
-    const tab = await getCurrentTab();
-    const allTabs = await getAllTabs();
-    const allTabsIds = allTabs.map(tab => tab.id);
-    await removeAllExceptCurrentTab(allTabsIds, tab.id);
+    await close_all_other_tabs();
+  } else if (command == 'close_duplicated_tabs') {
+    await close_duplicated_tabs();
   }
 });
 
