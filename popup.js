834342883e372b985e9d48b0416b0e75f59a28ae
Franco Serio <franco@samser.co>
2023-10-11 21:52:10 -0300

    get popup descriptions from manifest

--- popup.js
+++ popup.js
@@ -5,13 +5,13 @@ window.onload = async function() {
   const go_to_next_tab_command = allCommands.find(command => command.name === "go_to_next_tab");
   const go_to_previous_tab_command = allCommands.find(command => command.name === "go_to_previous_tab");
 
-  const close_all_tabs_except_current = close_all_tabs_except_current_command.shortcut;
-  const close_duplicated_tabs = close_duplicated_tabs_command.shortcut;
-  const go_to_next_tab = go_to_next_tab_command.shortcut;
-  const go_to_previous_tab = go_to_previous_tab_command.shortcut;
+  document.getElementById('close-all-except-current-command').textContent = close_all_tabs_except_current_command.shortcut;
+  document.getElementById('close-duplicated-command').textContent = close_duplicated_tabs_command.shortcut;
+  document.getElementById('go-to-next-tab-command').textContent = go_to_next_tab_command.shortcut;
+  document.getElementById('go-to-previous-tab-command').textContent = go_to_previous_tab_command.shortcut;
 
-  document.getElementById('close-all-except-current-command').textContent = close_all_tabs_except_current;
-  document.getElementById('close-duplicated-command').textContent = close_duplicated_tabs;
-  document.getElementById('go-to-next-tab-command').textContent = go_to_next_tab;
-  document.getElementById('go-to-previous-tab-command').textContent = go_to_previous_tab;
+  document.getElementById('close-all-except-current-description').textContent = close_all_tabs_except_current_command.description;
+  document.getElementById('close-duplicated-description').textContent = close_duplicated_tabs_command.description;
+  document.getElementById('go-to-next-tab-description').textContent = go_to_next_tab_command.description;
+  document.getElementById('go-to-previous-tab-description').textContent = go_to_previous_tab_command.description;
 }
