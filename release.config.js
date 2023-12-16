54ac92a8ed5477e3f506d9ac55a25c38e2c95fb2
Franco Serio <franco@samser.co>
2023-12-16 18:56:34 -0300

    ci: Adds semantic-release

---
+++ release.config.js
@@ -0,0 +1,15 @@
+const config = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    ['@semantic-release/git', {
+      'assets': ['samext.zip'],
+      'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+    }],
+    '@semantic-release/github'
+  ]
+};
+
+module.exports = config;
