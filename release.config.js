c1bd57c23e9f7c120efe01f329c5fc95ce366900
Franco Serio <franco@samser.co>
2023-12-16 19:19:43 -0300

    fix: remove npm registry from release workflow

--- release.config.js
+++ release.config.js
@@ -3,7 +3,6 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
     ['@semantic-release/git', {
       'assets': ['samext.zip'],
       'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
