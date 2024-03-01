054961be8544eadf5c91268c5bbd90c096a780b8
Franco Serio <franco@samser.co>
2024-03-01 11:30:24 +0100

    feat: add semantic-relase/npm

--- release.config.js
+++ release.config.js
@@ -3,6 +3,9 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    ['@semantic-release/npm', {
+      "nmpPublish": false
+    }],
     ['@semantic-release/git', {
       'assets': ['samext.zip'],
       'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
