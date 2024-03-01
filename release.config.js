37a419acddb51ad5644d6dbec207adf1ab0b729f
Franco Serio <franco@samser.co>
2024-03-01 21:14:25 +0100

    fix: remove semantic-release/npm and bump version

--- release.config.js
+++ release.config.js
@@ -3,9 +3,6 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ['@semantic-release/npm', {
-      "nmpPublish": false
-    }],
     ['@semantic-release/git', {
       'assets': ['samext.zip'],
       'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
