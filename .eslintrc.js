85accdacd8c41d68ca92bd4a29c20947503512bf
Franco Serio <franco@samser.co>
2025-09-06 21:09:23 +0200

    fix: add amd and node environments to ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,9 @@
 module.exports = {
   "env": {
     "browser": true,
-    "es2021": true
+    "es2021": true,
+    "amd": true,
+    "node": true
   },
   "extends": "eslint:recommended",
   "parserOptions": {
