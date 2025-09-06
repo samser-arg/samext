0d263aa276ac6a4c3e043487b1e4b0475ad304d4
Franco Serio <franco@samser.co>
2025-09-06 21:14:40 +0200

    feat: add safari extension

---
+++ samext-safari/samext-safari/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  samext-safari
+//
+//  Created by Franco Serio on 06/09/2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
