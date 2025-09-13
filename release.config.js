13c9fcffb38c6f6ad74ed6e598a7b1c5c6d5bec0
Franco Serio <franco@samser.co>
2025-09-13 17:32:17 +0200

    fix: trying compressing

--- release.config.js
+++ release.config.js
@@ -4,13 +4,13 @@ const config = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     ['@semantic-release/git', {
-      'assets': ['samext.zip', 'samext-safari.app', 'package.json', 'package-lock.json'],
+      'assets': ['samext.zip', 'samext-safari.app.zip', 'package.json', 'package-lock.json'],
       'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
     }],
     ['@semantic-release/github', {
       'assets': [
         { 'path': 'samext.zip', 'label': 'Chrome Extension (Minified)' },
-        { 'path': 'samext-safari/samext-safari.app', 'label': 'Safari Extension (macOS App)' }
+        { 'path': 'samext-safari/samext-safari.app.zip', 'label': 'Safari Extension (macOS App)' }
       ]
     }]
   ]
