7dfd4008ad40a16ed204afe8f2e64785494c3e3a
Franco Serio <franco@samser.co>
2025-09-13 09:59:53 +0200

    fix: try bundle app instead of zip

--- release.config.js
+++ release.config.js
@@ -4,13 +4,13 @@ const config = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     ['@semantic-release/git', {
-      'assets': ['samext.zip', 'samext-safari.zip'],
+      'assets': ['samext.zip', 'samext-safari.app'],
       'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
     }],
     ['@semantic-release/github', {
       'assets': [
         { 'path': 'samext.zip', 'label': 'Chrome Extension (Minified)' },
-        { 'path': 'samext-safari/samext-safari.zip', 'label': 'Safari Extension (macOS App)' }
+        { 'path': 'samext-safari/samext-safari.app', 'label': 'Safari Extension (macOS App)' }
       ]
     }]
   ]
