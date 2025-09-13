bd302d8e5a9cbb0905e0aa44843851e3debab461
Franco Serio <franco@samser.co>
2025-09-13 19:09:20 +0200

    fix: try not stripping Contents

--- release.config.js
+++ release.config.js
@@ -7,13 +7,13 @@ const config = {
       'npmPublish': false
     }],
     ['@semantic-release/git', {
-      'assets': ['samext.zip', 'samext-safari.app.zip', 'package.json', 'package-lock.json'],
+      'assets': ['samext.zip', 'DerivedData/Build/Products/Release/samext-safari.app.zip', 'package.json', 'package-lock.json'],
       'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
     }],
     ['@semantic-release/github', {
       'assets': [
         { 'path': 'samext.zip', 'label': 'Chrome Extension (Minified)' },
-        { 'path': 'samext-safari/samext-safari.app.zip', 'label': 'Safari Extension (macOS App)' }
+        { 'path': 'DerivedData/Build/Products/Release/samext-safari.app.zip', 'label': 'Safari Extension (macOS App)' }
       ]
     }]
   ]
