7bcc2b4417a5b8e95b8677c8c3a81020f7d3fca9
Franco Serio <franco@samser.co>
2025-09-14 18:19:26 +0200

    fix: trying ditto instead of zip

--- release.config.js
+++ release.config.js
@@ -7,13 +7,13 @@ const config = {
       'npmPublish': false
     }],
     ['@semantic-release/git', {
-      'assets': ['samext.zip', 'DerivedData/Build/Products/Release/samext-safari.app.zip', 'package.json', 'package-lock.json'],
+      'assets': ['samext.zip', 'samext-safari.zip', 'package.json', 'package-lock.json'],
       'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
     }],
     ['@semantic-release/github', {
       'assets': [
         { 'path': 'samext.zip', 'label': 'Chrome Extension (Minified)' },
-        { 'path': 'DerivedData/Build/Products/Release/samext-safari.app.zip', 'label': 'Safari Extension (macOS App)' }
+        { 'path': 'samext-safari/samext-safari.zip', 'label': 'Safari Extension (macOS App)' }
       ]
     }]
   ]
